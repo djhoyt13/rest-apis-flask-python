c88eca22dd72cc3a46d3cc9112bac7a0b26d4063
djhoyt13 <djhoyt13@gmail.com>
2023-10-05 15:33:44 -0400

    Finished mod 1

--- project/dj_projects/app.py
+++ project/dj_projects/app.py
@@ -18,16 +18,14 @@ stores = [
 def get_stores():
     return {"stores": stores}
 
+
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
-    new_store = {"name": request_data["name"], "item": []}
+    new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
 
-'''
-Creat Items in each store
-'''
 
 @app.post("/store/<string:name>/item")
 def create_item(name):
@@ -37,4 +35,20 @@ def create_item(name):
             new_item = {"name": request_data["name"], "price": request_data["price"]}
             store["items"].append(new_item)
             return new_item, 201
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
     return {"message": "Store not found"}, 404
